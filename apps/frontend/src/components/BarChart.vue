b4f5ce80efe154db867342d9e4c7323d3ee7a3a3
crypto0627 <jake0627a1@gmail.com>
2024-12-21 22:29:03 +0800

    feat: allowedLabels

--- apps/frontend/src/components/BarChart.vue
+++ apps/frontend/src/components/BarChart.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="loading" class="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-xl mt-8 text-center">
-    <p>數據加載中...</p>
+    <p>Loading...</p>
   </div>
   <div v-else class="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-xl mt-8">
     <Bar v-if="chartKey" :data="chartData" :options="chartOptions" :key="chartKey" ref="barChart" />
@@ -24,6 +24,7 @@ import type { ChartData } from '../types/types'
 import { rawData } from '../constants/InitData'
 import eventBus from '../services/eventBus'
 import type { ChartOptions, FontSpec } from 'chart.js'
+import { allowedLabels } from '../constants/allowedLabels'
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
@@ -71,7 +72,6 @@ export default defineComponent({
             callback: (tickValue: string | number) => {
               const allowedValues = [1200, 1000, 500, 0, -500, -1064]
 
-              // 確保 tickValue 是 number 類型再進行比較
               if (typeof tickValue === 'number' && allowedValues.includes(tickValue)) {
                 return tickValue.toString()
               }
@@ -103,19 +103,27 @@ export default defineComponent({
     })
 
     const updateChartData = (newData: typeof rawData.value) => {
-      chartData.value.labels = newData.map((item) => item.time)
-      chartData.value.datasets[0].data = newData.map((item) => item.data)
-      chartData.value.datasets[0].backgroundColor = newData.map((item) =>
-        item.data > 0 ? 'green' : 'red',
+      const filteredLabels = allowedLabels.filter((label) =>
+        newData.some((item) => item.time === label),
       )
-      chartData.value.datasets[0].borderColor = newData.map((item) =>
-        item.data > 0 ? 'green' : 'red',
+
+      const filteredData = filteredLabels.map((label) => {
+        const item = newData.find((data) => data.time === label)
+        return item ? item.data : null
+      })
+
+      chartData.value.labels = filteredLabels
+      chartData.value.datasets[0].data = filteredData.map((d) => d ?? 0)
+      chartData.value.datasets[0].backgroundColor = filteredData.map((data) =>
+        data ? (data > 0 ? 'green' : 'red') : 'gray',
       )
+      chartData.value.datasets[0].borderColor = filteredData.map((data) =>
+        data ? (data > 0 ? 'green' : 'red') : 'gray',
+      )
+
       chartKey.value++
-      console.log(chartData.value.labels)
     }
 
-    // 模擬 API 請求
     const fetchData = async () => {
       const apiUrl = `${import.meta.env.VITE_SCHEDULE_DATA_API_URL}/api/data`
       const dataFetcher = new DataFetcher(apiUrl)
